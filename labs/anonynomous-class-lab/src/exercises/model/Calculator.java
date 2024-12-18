5d01180a4bd73d9b9d3207c65d9312e85ba25491
Michael <michael@gmail.com>
2024-12-18 11:29:56 +0100

    tasks done

---
+++ labs/anonynomous-class-lab/src/exercises/model/Calculator.java
@@ -0,0 +1,5 @@
+package exercises.model;
+
+public interface Calculator {
+    public int calculate(int a, int b);
+}
---
+++ labs/anonynomous-class-lab/src/exercises/model/UserValidation.java
@@ -0,0 +1,5 @@
+package exercises.model;
+
+public interface UserValidation {
+    public boolean validate(String username, String password);
+}
